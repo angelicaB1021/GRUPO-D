0a8b67808c053b38e9a7cd0f6a2fc672a4e8fa12
Les138 <138824809+Les138@users.noreply.github.com>
2024-09-19 21:08:03 -0500

    CategoriaAccesorios

--- pet-Ecommerce/src/App.jsx
+++ pet-Ecommerce/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Categorias, categorias } from "./page/categorias/Categorias";
 import {
+  Categorias,
   //CategoriasDetalles, 
   CategoriaAccesorios,
   CategoriaAlimento,
